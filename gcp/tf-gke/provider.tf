8b19f879fea9a0cc677f6ee9b4ac1a84af01f3dc
vij_kes <97831607+vijkes@users.noreply.github.com>
2024-08-08 12:30:26 +0530

    Create provider.tf

---
+++ gcp/tf-gke/provider.tf
@@ -0,0 +1,19 @@
+provider "google" {
+    credentials = file("/home/hpuser/.config/gcloud/application_default_credentials.json")
+    project = "devsecops-431009"
+    region = "us-west4"
+
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "gke-cicd-july"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "~> 4.0"
+    }
+  }
+}
